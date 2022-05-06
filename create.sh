72d31bb2756b6618b2b0dad6f80359d485e80b5a
acerock1 <afolabi.fajobi@lions.lincoln.edu>
2022-05-06 16:42:41 +0000

    this is a draft and my first time using git

---
+++ create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Welcome to the creation script"
+sleep 2
+echo 
+echo
+echo "I can create a new user for you"
+echo
+echo
+sleep 1
+echo "Do you want me to proceed? Yes or no"
+echo
+sleep 1
+read answer
+sleep 1
+echo
+echo
+
