f1f81419957722df77d306dc1625c46a85180783
acerock1 <afolabi.fajobi@lions.lincoln.edu>
2022-05-10 18:27:58 +0000

    cleaned some errors

--- create.sh
+++ create.sh
@@ -22,23 +22,8 @@ echo
 <<<<<<< HEAD
 if [ $answer = yes ]
 	then 
-		useradd -m $nu 
+		useradd -m $nu
+	       echo "You have created user $nu, please check home directory"	
 	else
 		echo "Thank you for your time"
 fi
-echo
-=======
-echo "You chose $answer"
-echo
-if [ $answer = yes]
-	then
-		echo "What is your name?"
-		echo
-		read $name'
-		useradd -m $name
-		echo "The user $name has been created"
-	else
-		echo "Thank you for your time"
-fi
-
->>>>>>> ef1a3e288645d4edc8b1be52e9d7aa9e1d1149b5
